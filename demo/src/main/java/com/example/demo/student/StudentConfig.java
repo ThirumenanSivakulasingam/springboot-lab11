bba49ad3f47604674b3ceea322072b1e495b0c1d
Thirumenan S. <e21412@eng.pdn.ac.lk>
2025-07-16 23:55:21 +0530

    database is working

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args ->{
+                    Student thirumenan = new Student(
+                            "Thirumenan",
+                            "e21412@eng.pdn.ac.lk",
+                            23,
+                            LocalDate.of(2002, Month.JANUARY, 5)
+                    );
+
+                    Student tithurshan = new Student(
+                            "Tithurshan",
+                            "e21413@eng.pdn.ac.lk",
+                            23,
+                            LocalDate.of(2002, Month.DECEMBER, 28)
+                    );
+
+                    repository.saveAll(List.of(thirumenan, tithurshan));
+            };
+        };
+    }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,13 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        412L,
-                        "Thirumenan",
-                        "e21412@eng.pdn.ac.lk",
-                        23,
-                        LocalDate.of(2002, Month.JANUARY, 5)
-                )
-        );
+        return studentRepository.findAll();
     }
 }
